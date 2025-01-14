22072f09572d68263ed610bc0cadb167c78cb1f2
Leonardo Malikoski <uleonardomalikoski@gmail.com>
2025-01-13 22:03:52 -0300

    initial commit

---
+++ src/main/java/com/uleoo/ForumHubOracleOne/ForumHubOracleOneApplication.java
@@ -0,0 +1,13 @@
+package com.uleoo.ForumHubOracleOne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForumHubOracleOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForumHubOracleOneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uleoo/ForumHubOracleOne/ForumHubOracleOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uleoo.ForumHubOracleOne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumHubOracleOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
